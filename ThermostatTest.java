ef45bc8e0292921fa02cc7553d2121071a9c7250
TomasGalindo <tgalindoencinas@yahoo.com>
2018-01-01 19:00:39 +0100

    añadido inicializacion de las variables en los test

--- ThermostatTest.java
+++ ThermostatTest.java
@@ -2,18 +2,35 @@ package ejercicio2;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ThermostatTest {
-
-	@Test
-	public void Inicializacion_partOfDaytest() {
-		assertTrue("wrong inicialization", Thermostat.partOfDay.equals("Wake"));
-	}
-	@Test
-	public void Inicializacion_temptest() {
-		assertTrue("wrong inicialization", Thermostat.temp.equals("Low"));
+	
+	@Before
+	public void InitStreams(){
+		Thermostat.partOfDay = "Wake";
+		Thermostat.temp = "Low";
 	}
 	
+	@Test
+	public void Init_partOfDaytest() {
+		assertTrue("wrong init", Thermostat.partOfDay.equals("Wake"));
+	}
+	@Test
+	public void Init_temptest() {
+		assertTrue("wrong init", Thermostat.temp.equals("Low"));
+	}
+	@Test
+	public void changePartOfDaytest(){
+		Thermostat.advance();
+		assertTrue("wrong change", Thermostat.partOfDay.equals("Sleep"));
+	}
+	
+	@Test
+	public void changeTemptest(){
+		Thermostat.up();
+		assertTrue("wrong change", Thermostat.temp.equals("High"));
+	}
 
 }
